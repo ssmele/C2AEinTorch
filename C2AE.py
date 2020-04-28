b719545bb9682fb641f4cb4f4362c28f9fd58c80
ssmele <ssmele1995@gmail.com>
2020-04-28 14:23:00 -0600

    Training, and experiments

--- C2AE.py
+++ C2AE.py
@@ -9,35 +9,26 @@ class Fd(torch.nn.Module):
 
     def __init__(self, in_dim, H, out_dim, fin_act=None):
         super(Fd, self).__init__()
-        self.dropout = torch.nn.Dropout(0.25)
         self.fc1 = torch.nn.Linear(in_dim, H)
         self.fc2 = torch.nn.Linear(H, out_dim)
         self.fin_act = fin_act
 
     def forward(self, x):
-#         x = self.dropout(F.leaky_relu(self.fc1(x), .1))
-#         x = self.fc2(x)
-        x = F.leaky_relu(self.fc1(x), .1)
+        x = F.leaky_relu(self.fc1(x))
         x = self.fc2(x)
         return self.fin_act(x) if self.fin_act else x
 
 
 class Fx(torch.nn.Module):
-
     def __init__(self, in_dim, H1, H2, out_dim):
         super(Fx, self).__init__()
-        self.dropout = torch.nn.Dropout(0.25)
         self.fc1 = torch.nn.Linear(in_dim, H1)
         self.fc2 = torch.nn.Linear(H1, H2)
         self.fc3 = torch.nn.Linear(H2, out_dim)
-
     def forward(self, x):
-#         x = self.dropout(F.leaky_relu(self.fc1(x), 0.1))
-#         x = self.dropout(F.leaky_relu(self.fc2(x), 0.1))
-#         x = self.dropout(F.leaky_relu(self.fc3(x), 0.1))
-        x = F.leaky_relu(self.fc1(x), 0.1)
-        x = F.leaky_relu(self.fc2(x), 0.1)
-        x = F.leaky_relu(self.fc3(x), 0.1)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
         return x
 
 
@@ -45,21 +36,18 @@ class Fe(torch.nn.Module):
 
     def __init__(self, in_dim, H, out_dim):
         super(Fe, self).__init__()
-        self.dropout = torch.nn.Dropout(0.25)
         self.fc1 = torch.nn.Linear(in_dim, H)
         self.fc2 = torch.nn.Linear(H, out_dim)
 
     def forward(self, x):
-#         x = self.dropout(F.leaky_relu(self.fc1(x), 0.1))
-#         x = self.dropout(F.leaky_relu(self.fc2(x), 0.1))
-        x = F.leaky_relu(self.fc1(x), 0.1)
-        x = F.leaky_relu(self.fc2(x), 0.1)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
         return x
 
 
 class C2AE(torch.nn.Module):
 
-    def __init__(self, Fx, Fe, Fd, alpha=.5, emb_lambda=.5, latent_dim=6,
+    def __init__(self, Fx, Fe, Fd, beta=1, alpha=.5, emb_lambda=.5, latent_dim=6,
                  device=None):
         super(C2AE, self).__init__()
         # Define main network components.
@@ -72,6 +60,7 @@ class C2AE(torch.nn.Module):
 
         # Hyperparam used to set tradeoff between latent loss, and corr loss.
         self.alpha = alpha
+        self.beta = beta
         # Lagrange to use in embedding loss.
         self.emb_lambda = emb_lambda
         self.latent_I = torch.eye(latent_dim).to(device)
@@ -85,7 +74,6 @@ class C2AE(torch.nn.Module):
             computes latent loss (MSE between feature maps). Then z = Fe(y) is
             sent through decoder network in which it tries to satisfy
             correlation equation.
-            TODO: ADD MORE DESCRIPTION HERE
 
         Testing:
             Simply runs feature vector x through autoencoder. Fd(Fx(x))
@@ -115,8 +103,9 @@ class C2AE(torch.nn.Module):
     def corr_loss(self, preds, y):
         """This method compares the predicted probabilitie class distribution
         from the decoder, with the true y labels.
-
-        Does this by [BLAH]
+        
+        How to vectorize this computation was inspired from this implementation:
+        https://github.com/hinanmu/C2AE_tensorflow/blob/master/src/network.py
         """
         # Generate masks for [0,1] elements.
         ones = (y == 1)
@@ -131,9 +120,7 @@ class C2AE(torch.nn.Module):
         diff_matrix = torch.exp(-(preds[:, :, None] - preds[:, None, :]))
         # This will sum all contributes to loss for each datapoint.
         losses = torch.flatten(diff_matrix*ix_matrix, start_dim=1).sum(dim=1)
-        # Normalize each loss
-#         print(torch.isnan(ones.sum(dim=1)).sum(), torch.isnan(zeros.sum(dim=1)).sum(), 'card')
-#         print((ones.sum(dim=1)*zeros.sum(dim=1) == 0).sum(), 'zero_sum')
+        # Normalize each loss add small epsilon incase 0.
         losses /= (ones.sum(dim=1)*zeros.sum(dim=1) + 1e-4)
         # Replace inf, and nans with 0.
         losses[losses == float('Inf')] = 0
@@ -188,17 +175,19 @@ def load_model(model_cls, path, *args, **kwargs):
     return model
 
 
-def eval_metrics(mod, metrics, datasets, device):
+def eval_metrics(mod, metrics, datasets, device, apply_sig=False):
     res_dict = {}
     for ix, dataset in enumerate(datasets):
         mod.eval()
-        x = dataset.tensors[0].to(device)
+        x = dataset.tensors[0].to(device).float()
         # Make predictions.
         preds = mod(x)
         # Convert them to binary multilabels.
-        y_pred = torch.round(preds).cpu().detach().numpy()
+        if apply_sig:
+            y_pred = torch.round(torch.sigmoid(preds)).cpu().detach().numpy()
+        else:
+            y_pred = torch.round(preds).cpu().detach().numpy()
         y_true = dataset.tensors[1].cpu().detach().numpy()
         # Calculate metric.
-        res_dict[f'dataset_{ix}'] = {metric.__name__: metric(
-            y_true, y_pred) for metric in metrics}
+        res_dict[f'dataset_{ix}'] = {metric.__name__: metric(y_true, y_pred) for metric in metrics}
     return res_dict
